3c43df4b52a7c3fd17f6e7722f7fd59c1fc22ebd
Brian Taylor <el.wubo@gmail.com>
2012-07-11 23:09:49 -0400

    use marshal for more generality

--- lib/rfpset.rb
+++ lib/rfpset.rb
@@ -4,7 +4,7 @@ require "rfpset/rfpset"
 module FPSet
   # Your code goes here...
   def to_file(data, filename)
-    array = Array(data.collect { |d| d.to_s })
+    array = Array(data.collect { |d| Marshal.dump(d) })
     result = FPSetInternal.spit_array(array, filename)
     raise "does the file #{filename} exist?" if result == -1
     return result
@@ -18,7 +18,7 @@ module FPSet
       names = array.join(", ")
       raise "do all files exist? (#{names})"
     end
-    return result
+    return result.map { |s| Marshal.load(s) }
   end
   module_function :intersect_files  
 end
